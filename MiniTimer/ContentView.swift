50945c305bfdd41e249da8cd1f84532e8bba07f6
Ricardo Sanchez <ricardo.sanchez@salesloft.com>
2020-06-24 22:05:39 -0500

    First commit

--- MiniTimer/ContentView.swift
+++ MiniTimer/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 10 ) {
+            Text("Hello, World!")
+            Button(action: {}) {
+                Text("▸")
+            }
+        }
     }
 }
 
